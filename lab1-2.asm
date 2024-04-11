7e109d407ce3a6353811232da23edafd90d49a88
yuliva <akalivaty@gmail.com>
2024-04-11 23:52:57 +0800

    1.modified branch name

--- lab1-2.asm
+++ lab1-2.asm
@@ -1,7 +1,11 @@
     AREA ArrayData, DATA, READWRITE
 
 ArrayA
-    DCD -10, 11, 20, 50, -20, -3 ; Declare an array of 6 elements
+    dcd -10, 11, 20, 50, -20, -3 ; Declare an array of 6 elements
+
+ArrayB space 24
+
+ArrayC space 24
 
     AREA Matrix, CODE, READONLY
 
@@ -11,56 +15,61 @@ num equ 6   ; set the number of elements in the array
 
 main
     ldr r0, =ArrayA
+    ldr r1, =ArrayB
+    ldr r2, =ArrayC
+
+    ;
 
     ; sum signed numbers
-    mov r1, #num ; loop times
+    mov r3, #num ; loop times
     mov r7, #0 ; signed sum register
     mov r5, #0 ; set 1 if overflow (check V flag)
-    add r2, pc, #4
-    str r2, [sp]
-    bl add_signed_sum
+    ; record the return address
+    add r4, pc, #4
+    str r4, [sp]
+    bl sum_signed_numbers
 
     ; sum unsigned numbers
-    mov r1, #num ; loop times
+    mov r3, #num ; loop times
     mov r8, #0 ; unsigned sum register
     mov r6, #0 ; set 1 if overflow (check C flag)
-    bl add_unsigned_sum
+    bl sum_unsigned_numbers
 
     b stop
 
-add_signed_sum
+sum_signed_numbers
     ldr r9, [r0], #4 ; r9 = *r0, r0 = r0 + 4
 
     ; if (r9 < 0) do
     adds r9, r9, #0 ; check if r9 is negative
     mvnmi r9, r9 ; if r9 is negative, r9 = ~r9 (1's complement)
     addmi r9, r9, #1 ; if r9 is negative, r9 = r9 + 1 (2's complement)
-    blmi negative
+    blmi negative_number
 
     ; else do
-    blpl positive
+    blpl positive_number
 
     movvs r5, #1 ; move 1 to r5 if overflow
 
-    subs r1, r1, #1 ; decrement loop times
-    bne add_signed_sum ; loop if loop times are not zero
+    subs r3, r3, #1 ; decrement loop times
+    bne sum_signed_numbers ; loop if loop times are not zero
     ldr pc, [sp] ; branch to main function if loop times are zero
 
-negative
+negative_number
     subs r7, r7, r9
     add lr, lr, #4 ; branch to `movvs r5, #1`
     mov pc, lr
 
-positive
+positive_number
     adds r7, r7, r9
     mov pc, lr
 
-add_unsigned_sum
+sum_unsigned_numbers
     ldr r9, [r0], #4 ; r9 = *r0, r0 = r0 + 4
     adds r8, r8, r9
     movcs r6, #1 ; move 1 to r6 if overflow
-    subs r1, r1, #1 ; decrement loop times
-    bne add_unsigned_sum ; loop if loop times are not zero
+    subs r3, r3, #1 ; decrement loop times
+    bne sum_unsigned_numbers ; loop if loop times are not zero
     mov pc, lr ; branch to main function if loop times are zero
 
     
