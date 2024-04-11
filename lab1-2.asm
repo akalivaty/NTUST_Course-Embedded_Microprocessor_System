54b1edf141efeea552e80204e819dccc2bd1a9f6
yuliva <akalivaty@gmail.com>
2024-04-11 23:30:07 +0800

    finish 2 sum problems

--- lab1-2.asm
+++ lab1-2.asm
@@ -9,37 +9,59 @@ num equ 6   ; set the number of elements in the array
 
     ENTRY
 
-start
+main
     ldr r0, =ArrayA
 
+    ; sum signed numbers
     mov r1, #num ; loop times
     mov r7, #0 ; signed sum register
-    mov r5, #0 ; set 1 if overflow
+    mov r5, #0 ; set 1 if overflow (check V flag)
+    add r2, pc, #4
+    str r2, [sp]
     bl add_signed_sum
 
-    ; ldr r0, =ArrayA
-    ; mov r1, #num ; loop times
-    ; mov r8, #0 ; unsigned sum register
-    ; mov r6, #0 ; set 1 if overflow
-    ; bl add_unsigned_sum
+    ; sum unsigned numbers
+    mov r1, #num ; loop times
+    mov r8, #0 ; unsigned sum register
+    mov r6, #0 ; set 1 if overflow (check C flag)
+    bl add_unsigned_sum
 
     b stop
 
 add_signed_sum
     ldr r9, [r0], #4 ; r9 = *r0, r0 = r0 + 4
-    adds r7, r7, r9 ; sum with signed
-    ; movvs r5, #1 ; move 1 to r5 if overflow
-    sub r1, r1, #1
-    bne add_signed_sum
+
+    ; if (r9 < 0) do
+    adds r9, r9, #0 ; check if r9 is negative
+    mvnmi r9, r9 ; if r9 is negative, r9 = ~r9 (1's complement)
+    addmi r9, r9, #1 ; if r9 is negative, r9 = r9 + 1 (2's complement)
+    blmi negative
+
+    ; else do
+    blpl positive
+
+    movvs r5, #1 ; move 1 to r5 if overflow
+
+    subs r1, r1, #1 ; decrement loop times
+    bne add_signed_sum ; loop if loop times are not zero
+    ldr pc, [sp] ; branch to main function if loop times are zero
+
+negative
+    subs r7, r7, r9
+    add lr, lr, #4 ; branch to `movvs r5, #1`
+    mov pc, lr
+
+positive
+    adds r7, r7, r9
     mov pc, lr
 
 add_unsigned_sum
     ldr r9, [r0], #4 ; r9 = *r0, r0 = r0 + 4
     adds r8, r8, r9
-    movvs r6, #1 ; move 1 to r6 if overflow
-    sub r1, r1, #1
-    bne add_unsigned_sum
-    mov pc, lr
+    movcs r6, #1 ; move 1 to r6 if overflow
+    subs r1, r1, #1 ; decrement loop times
+    bne add_unsigned_sum ; loop if loop times are not zero
+    mov pc, lr ; branch to main function if loop times are zero
 
     
 stop
